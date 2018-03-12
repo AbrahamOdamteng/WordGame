f94a00ecc2767e3acca201b5bd9b39c2844f3538
AbrahamOdamteng <abraham.odamteng.developer@gmail.com>
2018-03-12 17:55:02 +0000

    Provided unit tests pass

--- WordGame/WordGameService.cs
+++ WordGame/WordGameService.cs
@@ -1,17 +1,26 @@
 ﻿namespace WordGame
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class WordGameService : IWordGameService
     {
+        object lockObject = new object();
+        readonly IEnumerable<char> Letters;
+        IValidWords ValidWords;
+
         public WordGameService(string letters, IValidWords validWords)
         {
-            throw new NotImplementedException();
+            Letters = letters.OrderBy(c => c).ToArray();
+            ValidWords = validWords;
         }
 
         public WordGameService(char[] letters, IValidWords validWords)
         {
-            throw new NotImplementedException();
+            Letters = letters.OrderBy(c => c).ToArray();
+            ValidWords = validWords;
+
         }
 
         public string GetPlayerNameAtPosition(int position)
@@ -31,7 +40,36 @@
 
         public int? SubmitWord(string playerName, string word)
         {
-            throw new NotImplementedException();
+
+            if (!ValidWords.Contains(word))
+            {
+                return null;
+            }
+
+
+            if(CanMakeWordFromLetters(word))
+            {
+                return word.Count();
+            }
+            return null;
+
         }
+
+        bool CanMakeWordFromLetters(string word)
+        {
+            var stack = new Stack<char>(word.OrderByDescending(c => c));
+            foreach (var c in Letters)
+            {
+                if(c == stack.Peek())
+                {
+                    stack.Pop();
+                    if (!stack.Any()) return true;
+                }
+            }
+
+            if (stack.Any()) return false;
+            return true;
+        }
+
     }
 }
