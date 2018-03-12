9cfb3d90f2124e26b792cdc34b89989597dab0a4
AbrahamOdamteng <abraham.odamteng.developer@gmail.com>
2018-03-12 18:50:31 +0000

    Unit Tested all methods

--- WordGame/WordGameService.cs
+++ WordGame/WordGameService.cs
@@ -10,6 +10,9 @@
         readonly IEnumerable<char> Letters;
         IValidWords ValidWords;
 
+        LinkedList<LeaderBoardEntry> LeaderBoard = new LinkedList<LeaderBoardEntry>();
+
+
         public WordGameService(string letters, IValidWords validWords)
         {
             Letters = letters.OrderBy(c => c).ToArray();
@@ -25,34 +28,44 @@
 
         public string GetPlayerNameAtPosition(int position)
         {
-            throw new NotImplementedException();
+            if(position >= LeaderBoard.Count)
+            {
+                return null;
+            }
+            return LeaderBoard.ToArray()[position].PlayerName;
         }
 
         public int? GetScoreAtPosition(int position)
         {
-            throw new NotImplementedException();
+            if (position >= LeaderBoard.Count)
+            {
+                return null;
+            }
+            return LeaderBoard.ToArray()[position].Score;
         }
 
         public string GetWordEntryAtPosition(int position)
         {
-            throw new NotImplementedException();
+            if (position >= LeaderBoard.Count)
+            {
+                return null;
+            }
+            return LeaderBoard.ToArray()[position].Word;
         }
 
         public int? SubmitWord(string playerName, string word)
         {
-
             if (!ValidWords.Contains(word))
             {
                 return null;
             }
 
-
             if(CanMakeWordFromLetters(word))
             {
+                AddToLeaderBoard(new LeaderBoardEntry(playerName, word));
                 return word.Count();
             }
             return null;
-
         }
 
         bool CanMakeWordFromLetters(string word)
@@ -68,8 +81,50 @@
             }
 
             if (stack.Any()) return false;
+
             return true;
         }
 
+        void AddToLeaderBoard(LeaderBoardEntry entry)
+        {
+            if (!LeaderBoard.Any())
+            {
+                LeaderBoard.AddFirst(entry);
+            }
+            else
+            {
+                if(LeaderBoard.First.Value.Score < entry.Score)
+                {
+                    LeaderBoard.AddFirst(entry);
+                    return;
+                }
+
+                var current = LeaderBoard.Last;
+                
+                while (current != null)
+                {
+                    if (current.Value.Score >= entry.Score)
+                    {
+                        LeaderBoard.AddAfter(current, entry);
+                    }
+                    current = current.Previous;
+                }
+            }
+        }
+
+    }
+
+    struct LeaderBoardEntry
+    {
+        internal LeaderBoardEntry(string playerName,string word)
+        {
+            PlayerName = playerName;
+            Word = word;
+        }
+
+        internal readonly string PlayerName;
+        internal readonly string Word;
+
+        internal int Score { get { return Word.Length; } }
     }
 }
--- WordGameTests/WordGameServiceTest.cs
+++ WordGameTests/WordGameServiceTest.cs
@@ -25,5 +25,66 @@
             Assert.AreEqual(6, this.wordGameService.SubmitWord("player5", "woolly"));
             Assert.AreEqual(null, this.wordGameService.SubmitWord("player6", "adder"));
         }
+
+        [TestMethod]
+        public void TestGetPlayerNameAtPosition()
+        {
+            wordGameService.SubmitWord("player1", "all");
+            wordGameService.SubmitWord("player2", "word");
+            wordGameService.SubmitWord("player5", "woolly");
+
+            var firstPlayer = wordGameService.GetPlayerNameAtPosition(0);
+            Assert.AreEqual("player5", firstPlayer);
+
+            var secondPlayer = wordGameService.GetPlayerNameAtPosition(1);
+            Assert.AreEqual("player2", secondPlayer);
+
+            var lastPlayer = wordGameService.GetPlayerNameAtPosition(2);
+            Assert.AreEqual("player1", lastPlayer);
+
+            var nullPlayer = wordGameService.GetPlayerNameAtPosition(9);
+            Assert.IsNull(nullPlayer);
+        }
+
+        [TestMethod]
+        public void TestGetScoreAtPosition()
+        {
+            wordGameService.SubmitWord("player1", "all");
+            wordGameService.SubmitWord("player2", "word");
+            wordGameService.SubmitWord("player5", "woolly");
+
+            var topeScore = wordGameService.GetScoreAtPosition(0);
+            Assert.AreEqual(6, topeScore);
+
+            var secondBestScore = wordGameService.GetScoreAtPosition(1);
+            Assert.AreEqual(4, secondBestScore);
+
+            var lastPlaceScore = wordGameService.GetScoreAtPosition(2);
+            Assert.AreEqual(3, lastPlaceScore);
+
+            var nullScore = wordGameService.GetScoreAtPosition(9);
+            Assert.IsNull(nullScore);
+        }
+
+        [TestMethod]
+        public void TestGetWordEntryAtPosition()
+        {
+            wordGameService.SubmitWord("player1", "all");
+            wordGameService.SubmitWord("player2", "word");
+            wordGameService.SubmitWord("player5", "woolly");
+
+            var topWord = wordGameService.GetWordEntryAtPosition(0);
+            Assert.AreEqual("woolly", topWord);
+
+            var secondBestWord = wordGameService.GetWordEntryAtPosition(1);
+            Assert.AreEqual("word", secondBestWord);
+
+            var lastWord = wordGameService.GetWordEntryAtPosition(2);
+            Assert.AreEqual("all", lastWord);
+
+            var nullScore = wordGameService.GetWordEntryAtPosition(9);
+            Assert.IsNull(nullScore);
+        }
+
     }
 }
